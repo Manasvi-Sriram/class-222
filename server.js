cf43d84ba4bd114d654e67428ef40751a41b985d
Manasvi-Sriram <89577079+Manasvi-Sriram@users.noreply.github.com>
2022-07-13 18:39:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use("/peerjs", peerServer);
 var nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    port: 465,
+    port: 587,
     host: "smtp.gmail.com",
     auth: {
         user: '',
@@ -44,7 +44,7 @@ app.post("/send-mail", (req, res) => {
     const to = req.body.to;
     const url = req.body.url;
     const mailData = {
-        from: "apoorv.goyal@whitehatjr.com",
+        from: "manasvisriram1701@gmail.com",
         to: to,
         subject: "Join the video chat with me!",
         html: `<p>Hey there,</p><p>Come and join me for a video chat here - ${url}</p>`
